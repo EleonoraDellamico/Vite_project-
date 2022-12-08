d230bb59bd3dc21eca928b55199ba3abd5331693
Eleonora <eleonoradellamico4@gmail.com>
2022-12-08 12:49:39 +0100

    changed title and fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["eslint:recommended", "plugin:react/recommended"],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		"react/react-in-jsx-scope": 0,
+		"react/jsx-uses-react": 0,
+		eqeqeq: "warn",
+		strict: "off",
+		"react/no-set-state": "off",
+	},
+};
